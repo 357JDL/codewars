753c2c460edcb78fde2cf922250208a7d5026be7
357JDL <357JDL@gmail.com>
2023-02-26 21:50:53 -0800

    Add codewars for date ref filename

---
+++ 2023-02-26 (1).js
@@ -0,0 +1,10 @@
+// Even or Odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+
+function evenOrOdd(number) {
+  let x = number;
+  if( x % 2 === 0) return 'Even'
+  else return 'Odd'
+}
