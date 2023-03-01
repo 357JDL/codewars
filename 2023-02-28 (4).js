044b69a32c2af6e1785c52a2e18358762ae8ac44
357JDL <357JDL@gmail.com>
2023-02-28 21:51:24 -0800

    Add codewars for date ref filename

---
+++ 2023-02-28 (4).js
@@ -0,0 +1,17 @@
+// Double Char
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+
+function doubleChar(str) {
+    let newWord = []
+    str.split('').forEach(letter => {
+      newWord.push(letter)
+      newWord.push(letter)
+    })
+    return newWord.join('')
+  }
