df8951e9edc4d6be0e9696977c0549ab8a99bd39
357JDL <357JDL@gmail.com>
2023-03-04 21:52:59 -0800

    Add codewars for date ref filename

---
+++ 2023-03-04 (3).js
@@ -0,0 +1,9 @@
+// Remove String Spaces
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+
+function noSpace(x){
+    const splitBySpaces = x.split(' ')
+    return splitBySpaces.join('')
+  }
